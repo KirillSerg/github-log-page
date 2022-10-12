5af354351db6a756056fb53831a912c6676b2254
KirillSerg <kirill.sergienko1989@gmail.com>
2022-10-12 23:51:37 +0300

    added main structure

---
+++ src/components/Form.tsx
@@ -0,0 +1,28 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import "./Form.css";
+
+type Inputs = {
+  login: string,
+  password: string,
+};
+
+const Form = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
+  return (
+    <div className="form-section">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Username or email address</label>
+        <input {...register("login")} />
+        <label>Password</label>
+        <input {...register("password", { required: true })} />
+        {errors.password && <span>This field is required</span>}
+        
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
